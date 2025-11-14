220d7c45aecbfb4cf9859a6bb43e38527503c61b
trieu-h <lehuynhhaitrieu1996@gmail.com>
2025-11-14 14:54:49 -0700

    Add github workflow for deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
+import { router } from 'sv-router/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
+  plugins: [svelte(), tailwindcss(), router()],
   resolve: {
     alias: {
       $lib: path.resolve("./src/lib"),
     },
   },
+  base: "/popcorn-prophet/"
 })
