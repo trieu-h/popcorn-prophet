59a90b89f3f4d22b7e68341373807f9acc433664
Trieu Le <lehuynhhaitrieu1996@gmail.com>
2025-11-16 11:27:48 -0700

    Handle responsiveness for Prediction Result page

--- frontend/src/pages/PredictionResult.svelte
+++ frontend/src/pages/PredictionResult.svelte
@@ -22,8 +22,8 @@
     </div>
   </div>
 
-  <div class="flex justify-between gap-4 mb-5">
-    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2">
+  <div class="flex justify-between gap-4 mb-5 flex-col md:flex-row">
+    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2 min-w-0">
       <Card.Header>
         <Card.Title class="text-white">Projected Revenue</Card.Title>
       </Card.Header>
@@ -32,7 +32,7 @@
       </Card.Content>
     </Card.Root>  
 
-    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2">
+    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2 min-w-0">
       <Card.Header>
         <Card.Title class="text-white">Confidence</Card.Title>
       </Card.Header>
@@ -41,7 +41,7 @@
       </Card.Content>
     </Card.Root>  
 
-    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2">
+    <Card.Root class="flex-1 bg-blue-gray gap-4 border-blue-gray-2 min-w-0">
       <Card.Header>
         <Card.Title class="text-white">Verdict</Card.Title>
       </Card.Header>
@@ -53,39 +53,41 @@
 
   <p class="text-gray-2 self-center mb-12">Confidence interval: $135M - $165M</p>
   
-  <div class="bg-blue-gray border border-blue-gray-2 rounded-xl px-8 py-6 mb-10">
+  <div class="bg-blue-gray border border-blue-gray-2 rounded-xl px-8 py-6 mb-10 ">
     <h1 class="text-white font-bold text-xl mb-5">How It Compares</h1>
 
-    <table class="text-white w-full">
-      <thead class="text-left">
-        <tr class="border-b border-blue-gray-2">
-          <th class="px-4 py-3">Movie Title</th>
-          <th class="px-4 py-3">Genre</th>
-          <th class="px-4 py-3 text-right">Actual Box Office</th>
-          <th class="px-4 py-3 text-right">Vote Average</th>
-          <th class="px-4 py-3 text-right">Number of Votes</th>
-          <th class="px-4 py-3 text-right">Budget</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr class="border-b border-blue-gray-2">
-          <td class="px-4 py-5">Movie 1</td>
-          <td class="px-4 py-5">Sci-fi</td>
-          <td class="px-4 py-5 text-right">$270M</td>
-          <td class="px-4 py-5 text-right">7.8</td>
-          <td class="px-4 py-5 text-right">3400</td>
-          <td class="px-4 py-5 text-right">$45M</td>
-        </tr>
-        <tr class="border-b border-blue-gray-2">
-          <td class="px-4 py-5">Movie 2</td>
-          <td class="px-4 py-5">Adventure</td>
-          <td class="px-4 py-5 text-right">$20M</td>
-          <td class="px-4 py-5 text-right">7.8</td>
-          <td class="px-4 py-5 text-right">3400</td>
-          <td class="px-4 py-5 text-right">$45M</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="overflow-auto">
+      <table class="text-white w-full">
+        <thead class="text-left">
+          <tr class="border-b border-blue-gray-2">
+            <th class="px-4 py-3">Movie Title</th>
+            <th class="px-4 py-3">Genre</th>
+            <th class="px-4 py-3 text-right">Actual Box Office</th>
+            <th class="px-4 py-3 text-right">Vote Average</th>
+            <th class="px-4 py-3 text-right">Number of Votes</th>
+            <th class="px-4 py-3 text-right">Budget</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="border-b border-blue-gray-2">
+            <td class="px-4 py-5">Movie 1</td>
+            <td class="px-4 py-5">Sci-fi</td>
+            <td class="px-4 py-5 text-right">$270M</td>
+            <td class="px-4 py-5 text-right">7.8</td>
+            <td class="px-4 py-5 text-right">3400</td>
+            <td class="px-4 py-5 text-right">$45M</td>
+          </tr>
+          <tr class="border-b border-blue-gray-2">
+            <td class="px-4 py-5">Movie 2</td>
+            <td class="px-4 py-5">Adventure</td>
+            <td class="px-4 py-5 text-right">$20M</td>
+            <td class="px-4 py-5 text-right">7.8</td>
+            <td class="px-4 py-5 text-right">3400</td>
+            <td class="px-4 py-5 text-right">$45M</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 
   <div class="self-center flex gap-3">
