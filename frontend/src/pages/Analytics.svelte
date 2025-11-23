11714331ad47fc59f056ed35074420c1a1c7ee86
Trieu Le <lehuynhhaitrieu1996@gmail.com>
2025-11-22 21:13:02 -0700

    Improve usability of Search Movie Functionality

--- frontend/src/pages/Analytics.svelte
+++ frontend/src/pages/Analytics.svelte
@@ -9,7 +9,7 @@
   let movies: Movie[] = $state([]);
   let search_string = $state("");
   let is_loading = $state(false);
-  let last_movie_id_state: number | null = $state(null);
+  let last_movie_id_state: number | null = null;
 
   let timeout: number | null = null;
   const cooldown = 1000;
@@ -19,11 +19,18 @@
   }
 
   async function on_input_change() {
+    if (search_string.trim() === '') {
+      is_loading = false;
+      movies = [];
+      last_movie_id_state = null;
+      return;
+    }
+
     is_loading = true;
     if (timeout) clearTimeout(timeout);
       
     timeout = setTimeout(async () => {
-      await get_movies(search_string.trim());
+      await get_movies(search_string);
       is_loading = false;
     }, cooldown);
   }
@@ -35,23 +42,15 @@
     }
     const res = await fetch(url);
     const movies_json = await res.json();
-    if (last_movie_id) {
-      movies = [...movies, ...movies_json]
-    } else {
-      movies = movies_json;
-    }
-
-    if (movies.length) {
-      last_movie_id_state = movies[movies.length - 1].id;
-    }
+    movies = last_movie_id ? [...movies, ...movies_json] : movies_json;
+    last_movie_id_state = movies.length ? movies[movies.length - 1].id : null;
   }
 
   window.addEventListener("scroll", () => {
     if (window.innerHeight + window.scrollY >= document.documentElement.scrollHeight) {
-      get_movies(search_string, last_movie_id_state!);
+      get_movies(search_string.trim(), last_movie_id_state!);
     }
   })
-
 </script>
 
 <div class="max-w-4xl mx-auto pt-10 flex flex-col px-6">
@@ -62,39 +61,42 @@
     <CommandInput placeholder="Search for a movie title" oninput={on_input_change} bind:value={search_string}/>
   </Command>
 
-  <div class="grid grid-cols-2 md:grid-cols-4 gap-3">
-    {#if is_loading }
+  {#if is_loading}
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-3">
       {#each { length: 4 }}
-      <Card class="gap-3 opacity-50">
-        <CardHeader>
-          <CardTitle>
-            <Skeleton class="h-10 w-full bg-black"/>
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-         <Skeleton class="h-60 md:h-50 w-full bg-black"/>
-        </CardContent>
-      </Card>
-      {/each}
-    {:else}
-      {#if movies.length === 0 && search_string.trim().length !== 0}
-        <p class="text-white">No movies found</p>
-      {/if}
-      {#each movies as movie}
-        <Card class="gap-3" onclick={on_movie_selection}>
+        <Card class="gap-3 opacity-50">
           <CardHeader>
-            <CardTitle class="overflow-hidden text-ellipsis whitespace-nowrap">{movie.original_title}</CardTitle>
+            <CardTitle>
+              <Skeleton class="h-10 w-full bg-black"/>
+            </CardTitle>
           </CardHeader>
-          <CardContent class="text-center">
-            {#if movie.poster_path}
-              <img src={poster_prefix + movie.poster_path} alt="">
-            {:else}
-              <p>No poster available</p>
-              <p>(⋟﹏⋞)</p>
-            {/if}
+          <CardContent>
+           <Skeleton class="h-60 md:h-50 w-full bg-black"/>
           </CardContent>
         </Card>
       {/each}
+    </div>
+  {:else}
+    {#if movies.length === 0 && search_string.trim().length !== 0}
+      <p class="text-white text-3xl">No movies found (&gt;_&lt;)</p>
+    {:else}
+      <div class="grid grid-cols-2 md:grid-cols-4 gap-3">
+        {#each movies as movie}
+          <Card class="gap-3" onclick={on_movie_selection}>
+            <CardHeader>
+              <CardTitle class="overflow-hidden text-ellipsis whitespace-nowrap">{movie.original_title}</CardTitle>
+            </CardHeader>
+            <CardContent class="text-center">
+              {#if movie.poster_path}
+                <img src={poster_prefix + movie.poster_path} alt="">
+              {:else}
+                <p>No poster available</p>
+                <p>(⋟﹏⋞)</p>
+              {/if}
+            </CardContent>
+          </Card>
+        {/each}
+      </div>
     {/if}
-  </div>
+  {/if}
 </div>
