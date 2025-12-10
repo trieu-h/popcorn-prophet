e6986e55f4d55a58e6f9c61156db8ded820804e5
Trieu Le <lehuynhhaitrieu1996@gmail.com>
2025-12-09 23:18:50 -0700

    sticky header

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -13,7 +13,7 @@
 </script>
 
 <main>
-    <header class="text-white flex justify-between items-center max-md:items-start max-md:gap-3 border-b border-dark-gray p-5 max-md:flex-col">
+    <header class="text-white flex justify-between items-center max-md:items-start max-md:gap-3 border-b border-dark-gray p-5 max-md:flex-col sticky bg-dark-blue top-0 z-[2]">
       <div class="flex items-center gap-3">
         <img src={logo} class="w-10 h-10">
         <a class="hover:text-light-gray active:text-light-blue font-bold text-xl" href="/" use:isActiveLink={{ className: 'text-light-blue' }}>Popcorn Prophet</a>
