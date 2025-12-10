7af1aea5651bb0696a4acbb46ad23012ec26b975
Trieu Le <lehuynhhaitrieu1996@gmail.com>
2025-12-09 19:23:46 -0700

    resolve tailwind dynamic classes issue

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,7 +6,6 @@
   import { Button } from "$lib/components/ui/button/index.js";
   import ArrowUp from "@lucide/svelte/icons/arrow-up";
   import { analytics_state } from "./pages/states.svelte";
-  import { fade, fly } from 'svelte/transition';
 
   function go_to_top_of_page() {
     window.scrollTo({top: 0, behavior: "smooth"});
@@ -31,7 +30,7 @@
     <div id="overlay" class="fixed top-0 left-0 h-full w-full pointer-events-none">
       <Button 
         class="absolute rounded-4xl right-10 bottom-10 pointer-events-auto transition-opacity ease-in-out 
-               opacity-{!analytics_state.show_back_to_top_button ? "0" : "100"}"
+               {!analytics_state.show_back_to_top_button ? "opacity-0" : "opacity-100"}"
         size="icon" variant="outline" onclick={go_to_top_of_page} title="Back to top">
         <ArrowUp/>
       </Button>
